8b4ae5ce9de31612bf73f077bf323a4bceee6161
docker2110 <57570663+docker2110@users.noreply.github.com>
2020-09-14 13:02:36 +0530

    Add files via upload

---
+++ js/jfile1.js
@@ -0,0 +1,11 @@
+/*(function hello() {
+    alert('Hello!!!');
+})()*/
+function myTest() {
+    console.log("Welcome to My Test")
+    //alert('Welcome to custom js');
+}
+
+/*$(function() {
+    alert('Hello, custom js');
+});*/
